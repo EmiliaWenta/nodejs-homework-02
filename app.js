80b9b5219ada0c855fc6db70569efed631f903f2
Emilia Wenta <emiliakielinska96@gmail.com>
2024-02-27 10:59:05 +0100

    change static path

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ const formatsLogger = app.get("env") === "development" ? "dev" : "short";
 app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(process.cwd(), "./public")));
 
 setJWTStrategy();
 
