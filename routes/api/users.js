3a19e9335fca68fdaa5815badf9236aa83b76079
Emilia Wenta <emiliakielinska96@gmail.com>
2024-02-27 14:19:25 +0100

    Add rout patch/avatars

--- routes/api/users.js
+++ routes/api/users.js
@@ -5,6 +5,8 @@ import { logOut } from "../../controllers/users/logOut.js";
 import { logIn } from "../../controllers/users/logIn.js";
 import { signUp } from "../../controllers/users/signUp.js";
 import { current } from "../../controllers/users/current.js";
+import { updateUserAvatar } from "../../controllers/users/updateUserAvatar.js";
+import { uploadMiddleware } from "../../config/multerConfing.js";
 
 const router = express.Router();
 
@@ -15,5 +17,12 @@ router.post("/login", logIn);
 router.get("/logout", authMiddleware, logOut);
 
 router.get("/current", authMiddleware, current);
+
+router.patch(
+  "/avatars",
+  authMiddleware,
+  uploadMiddleware.single("avatar"),
+  updateUserAvatar
+);
 
 export { router };
